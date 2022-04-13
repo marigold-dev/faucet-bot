2ebb3e95b6cc7df5e39bda2d2e42ca177bc9a097
Felipe Cardozo <fraifelipe@gmail.com>
2022-04-13 15:07:47 -0300

    tezos network as env var

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ config();
 
 const TEZOS_SECRET_KEY = process.env.TEZOS_SECRET_KEY!;
 const TEZOS_RPC_URL = process.env.TEZOS_RPC_URL!;
+const TEZOS_NETWORK = process.env.TEZOS_NETWORK!;
 const PORT = process.env.PORT ?? 3000;
 // Set the RPC endpoint. If you're building a heavily-used app
 // you should run your own, but there's a list of publicly
@@ -39,10 +40,12 @@ app.get("/status", async (_, res) => {
       balance: await Tezos.tz.getBalance(address),
     }
 
+    const network = await Tezos.rpc.getRpcUrl();
+    console.log(network);
     console.log(`Address: ${result.address} - Balance: ${result.balance}`);
     res.send(result);
   } catch (error) {
-    console.error((error as any).message);
+    console.error((error as any));
     res.status(500).send(JSON.stringify(error, null, 2));
   }
 });
@@ -62,7 +65,7 @@ app.get("/getmoney/:address", async (req, res) => {
     await op.confirmation(1);
     console.log(`Confirmed - ${op.hash}`);
     res.send(
-      `Funds transferred. Check url for results: https://hangzhou.tzstats.com/${op.hash}\n`
+      `Funds transferred. Check url for results: https://${TEZOS_NETWORK}.tzstats.com/${op.hash}\n`
     );
   } catch (error) {
     console.error((error as any).message);
